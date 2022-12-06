b4822bb874f6871f1b2d50b0f164448d79a45983
Priyazalavadia <priyazalavadia@gmail.com>
2022-12-06 21:07:08 +0000

    project-1-maven-nopcommerce-sw-4-Priya

---
+++ src/main/java/com/nopcommerce/demo/pages/Homepage.java
@@ -0,0 +1,24 @@
+package com.nopcommerce.demo.pages;
+
+import com.nopcommerce.demo.utility.Utility;
+import org.openqa.selenium.By;
+
+public class Homepage extends Utility {
+
+
+    By loginLink = By.linkText("Log in");
+    By registerLink = By.linkText("Register");
+    By searchField = By.id("small-searchterms");
+
+    public void clickOnLoginLink() {
+        clickOnElement(loginLink);
+    }
+    public void clickOnRegisterLink(){
+        clickOnElement(registerLink);
+    }
+
+    public void enterTextToSearchField(String text){
+        sendTextToElement(searchField,"Mobile");
+    }
+
+}
